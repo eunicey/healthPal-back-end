59b4d7e8f3f201ef3378916a1d791707bd72e62d
Andrea <andrearewards@hotmail.com>
2023-10-16 17:54:56 -0400

    add factors to profile model

--- models/profile.js
+++ models/profile.js
@@ -3,8 +3,12 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
-  name: String,
-  photo: String
+  name: {type: String, required: true},
+  sex: {type: String, enum:["M", "F"]},
+  birthYear: {type: Number, required: true},
+  weight: {type: Number, required: true},
+  height: {type: Number, required: true},
+  medicalHx: {type: String, enum:["Hypertension", "Diabetes", "Hyperlipidemia", "Gastric Reflux", "Cancer", "Gout", "Other"]}
 },{
   timestamps: true,
 })

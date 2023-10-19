0746c7ae924fffe61eb2b74710246c2d55ecb016
Eunice Yang <euniceyang21@gmail.com>
2023-10-19 10:27:27 -0500

    Refactor api call

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,27 +1,38 @@
 import { Profile } from "../models/profile.js"
 
-const API= process.env.API_KEY
-const API_URL= 'https://api.openai.com/v1/chat/completions'
+const OPENAI_API_KEY= process.env.API_KEY
+const OPENAI_URL= 'https://api.openai.com/v1/chat/completions'
 
 
-async function create(req, res) {
+async function getResults(req, res) {
+
   try {
+
     const options = {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${API}`,
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: req.body.message}],
+        messages: [
+          {
+            role: "system",
+            content: "You are an AI medical assistant. Generate a structured response with the following sections: If symptoms require serious medical attention, respond with \"SOS call 911\", describe the best treatment option and what the user should do next, list the symptoms that led to the treatment recommended.",
+          },
+          {
+            role: "user", 
+            content: req.body.message,
+          }],
         max_tokens: 100,
       })
     }
 
-    const response = await fetch(`${API_URL}`, options)
-    const data = await response.json()
-    res.send(data)
+    const apiResponse = await fetch(`${OPENAI_URL}`, options)
+
+    const resultsData = await apiResponse.json()
+    res.send(resultsData)
 
   } catch (error){
     console.log(error)
@@ -30,5 +41,5 @@ async function create(req, res) {
 }
 
 export { 
-  create,
+  getResults,
 }
